b62cd82a039be1074d61121fd611fbdf2be3d084
Andrew Galante <drew@perfectco.com>
2022-07-19 10:54:57 -0700

    Basic client, can connect to server

---
+++ src/android/com/perfectco/cordova/TlsPskClient.java
@@ -0,0 +1,43 @@
+package com.perfectco.cordova;
+
+import org.bouncycastle.tls.PSKTlsClient;
+import org.bouncycastle.tls.TlsClient;
+import org.bouncycastle.tls.TlsClientProtocol;
+import org.bouncycastle.tls.crypto.TlsCrypto;
+import org.bouncycastle.tls.crypto.impl.bc.BcTlsCrypto;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.security.SecureRandom;
+import java.util.UUID;
+
+public class TlsPskClient {
+  private static final TlsCrypto crypto = new BcTlsCrypto(new SecureRandom());
+  private final TlsClient client;
+  private final UUID id = UUID.randomUUID();
+
+  private Socket socket;
+  private TlsClientProtocol protocol;
+
+  public TlsPskClient(byte[] key) {
+    client = createClient(key);
+  }
+
+  public UUID getId() { return id; }
+
+  public void connect(String host, int port) throws IOException {
+    socket = new Socket(host, port);
+    protocol = new TlsClientProtocol(socket.getInputStream(), socket.getOutputStream());
+
+    protocol.connect(client);
+  }
+
+  public void close() throws IOException {
+    protocol.close();
+    socket.close();
+  }
+
+  private static TlsClient createClient(byte[] key) {
+    return new PSKTlsClient(crypto, new byte[0], key);
+  }
+}
--- src/android/com/perfectco/cordova/TlsPskPlugin.java
+++ src/android/com/perfectco/cordova/TlsPskPlugin.java
@@ -1,13 +1,116 @@
 package com.perfectco.cordova;
 
+import android.util.Base64;
+import android.util.Log;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class TlsPskPlugin extends CordovaPlugin {
+  static final String TAG = "TlsPskPlugin";
+  static final String ACTION_CONNECT = "connect";
+  static final String ACTION_CLOSE = "close";
+  static final String ACTION_SEND = "send";
+  static final String ACTION_START = "start";
+  static final String ACTION_STOP = "stop";
+
+  HashMap<UUID, TlsPskClient> clients = new HashMap<>();
 
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
+    switch (action) {
+      case ACTION_CONNECT: {
+        String host;
+        try {
+          host = args.getString(0);
+        } catch (JSONException e) {
+          Log.e(TAG, e.getMessage(), e);
+          callbackContext.error("Hostname error");
+          return false;
+        }
+
+        int port;
+        try {
+          port = args.getInt(1);
+        } catch (JSONException e) {
+          Log.e(TAG, e.getMessage(), e);
+          callbackContext.error("Port error");
+          return false;
+        }
+
+        byte[] key;
+        try {
+          String base64 = args.getString(2);
+          key = Base64.decode(base64, Base64.DEFAULT);
+        } catch (JSONException e) {
+          Log.e(TAG, e.getMessage(), e);
+          callbackContext.error("Key error");
+          return false;
+        }
+
+        try {
+          UUID id = connect(host, port, key);
+          JSONObject status = new JSONObject();
+          status.put("uuid", id.toString());
+          PluginResult result = new PluginResult(PluginResult.Status.OK, status);
+          result.setKeepCallback(true);
+          callbackContext.sendPluginResult(result);
+        } catch (IOException | JSONException e) {
+          Log.e(TAG, e.getMessage(), e);
+          callbackContext.error("Connect error");
+          return false;
+        }
+      } return true;
+      case ACTION_CLOSE: {
+        UUID id;
+        try {
+          String sid = args.getString(0);
+          id = UUID.fromString(sid);
+        } catch (JSONException | IllegalArgumentException e) {
+          Log.e(TAG, e.getMessage(), e);
+          callbackContext.error("Unknown client");
+          return false;
+        }
+
+        try {
+          close(id);
+          callbackContext.success();
+        } catch (IOException e) {
+          Log.e(TAG, e.getMessage(), e);
+          callbackContext.error("Error closing");
+          return false;
+        }
+      } return true;
+      case ACTION_SEND:
+        break;
+      case ACTION_START:
+        break;
+      case ACTION_STOP:
+        break;
+    }
     return false;
+  }
+
+  private UUID connect(String host, int port, byte[] key) throws IOException {
+    TlsPskClient client = new TlsPskClient(key);
+    clients.put(client.getId(), client);
+    client.connect(host, port);
+    return client.getId();
+  }
+
+  private void close(UUID id) throws IOException {
+    TlsPskClient client = clients.get(id);
+    if (client != null) {
+      client.close();
+      clients.remove(id);
+    }
   }
 }
